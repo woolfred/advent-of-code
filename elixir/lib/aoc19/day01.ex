cce47768d5817b0472d008c10baae21b7b45e78b
Frederik Delißen <frederik.delissen@zweitag.de>
2019-12-02 13:44:04 +0100

    div/2 performs truncated integer division

--- elixir/lib/aoc19/day01.ex
+++ elixir/lib/aoc19/day01.ex
@@ -1,9 +1,6 @@
 defmodule AoC19.Day01 do
   def fuel_for(mass) when is_integer(mass) do
-    mass
-    |> div(3)
-    |> trunc()
-    |> Kernel.-(2)
+    div(mass, 3) - 2
   end
 
   def fuel_for(mass) when is_binary(mass) do
@@ -18,13 +15,9 @@ defmodule AoC19.Day01 do
     |> Enum.reduce(0, fn mass, acc -> acc + fuel_calc_fun.(mass) end)
   end
 
-  def all_fuel_for(mass) do
-    all_fuel_for(0, fuel_for(mass))
-  end
+  def all_fuel_for(mass), do: all_fuel_for(0, fuel_for(mass))
 
-  defp all_fuel_for(acc, mass) when mass <= 0 do
-    acc
-  end
+  defp all_fuel_for(acc, mass) when mass <= 0, do: acc
 
   defp all_fuel_for(acc, mass) do
     all_fuel_for(acc + mass, fuel_for(mass))
