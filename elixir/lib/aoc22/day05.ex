a3e72896ae2a55c09a03480316d8902d777c8681
Frederik Delißen <frederik.delissen@zweitag.de>
2022-12-05 18:40:52 +0100

    Day 5

---
+++ elixir/lib/aoc22/day05.ex
@@ -0,0 +1,55 @@
+defmodule AoC22.Day05 do
+  def part(input, fun) do
+    {crates, commands} = to_creates_and_commands(input)
+
+    Enum.reduce(commands, crates, fn [amount, from, to], crates ->
+      {taken, remaining} = Enum.split(crates[from], amount)
+
+      Map.put(crates, from, remaining)
+      |> Map.put(to, fun.(taken) ++ crates[to])
+    end)
+    |> top_crates()
+  end
+
+  def part_a(input), do: part(input, &Enum.reverse/1)
+  def part_b(input), do: part(input, & &1)
+
+  defp to_creates_and_commands(input) do
+    [crates, commands] = String.split(input, "\n\n")
+
+    {to_crates(crates), to_commands(commands)}
+  end
+
+  defp to_crates(input) do
+    input
+    |> String.split("\n")
+    |> Enum.flat_map(fn row ->
+      String.graphemes(row)
+      |> Enum.drop_every(2)
+      |> Enum.with_index()
+      |> dbg()
+      |> Enum.filter(fn {crate, _} -> Regex.match?(~r/[A-Z]/, crate) end)
+    end)
+    |> Enum.reverse()
+    |> Enum.reduce(%{}, fn {crate, col}, cratesmap ->
+      Map.update(cratesmap, to_string(div(col, 2) + 1), [crate], fn existing ->
+        [crate | existing]
+      end)
+    end)
+  end
+
+  defp to_commands(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn command ->
+      [_move, amount, _from, from, _to, to] = String.split(command)
+      [String.to_integer(amount), from, to]
+    end)
+  end
+
+  defp top_crates(crates) do
+    Map.keys(crates)
+    |> Enum.sort()
+    |> Enum.map_join(fn stack -> hd(crates[stack]) end)
+  end
+end
