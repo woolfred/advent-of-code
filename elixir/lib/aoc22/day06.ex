95f680d4d452a0f82519631e5768709a33770a9c
Frederik Delißen <frederik.delissen@zweitag.de>
2022-12-06 11:06:47 +0100

    Use Stream instead of recursion

--- elixir/lib/aoc22/day06.ex
+++ elixir/lib/aoc22/day06.ex
@@ -1,22 +1,14 @@
 defmodule AoC22.Day06 do
-  def part_a(input), do: find_marker(input, 4)
+  def part_a(input), do: find_unique_chars_marker(input, 4)
 
-  def part_b(input), do: find_marker(input, 14)
+  def part_b(input), do: find_unique_chars_marker(input, 14)
 
-  defp find_marker(input, nr_of_distinct_chars) do
-    find_marker(input, 0..(nr_of_distinct_chars - 1), nr_of_distinct_chars)
-  end
-
-  defp find_marker(input, range, nr_of_distinct_chars, done? \\ false)
-  defp find_marker(_input, a..b, _, true), do: b
-
-  defp find_marker(input, a..b = slice_range, nr_of_distinct_chars, _done?) do
-    set =
-      String.slice(input, slice_range)
-      |> String.split("", trim: true)
-      |> MapSet.new()
-
-    done = MapSet.size(set) == nr_of_distinct_chars
-    find_marker(input, (a + 1)..(b + 1), nr_of_distinct_chars, done)
+  defp find_unique_chars_marker(input, length) do
+    input
+    |> String.to_charlist()
+    |> Stream.chunk_every(length, 1, :discard)
+    |> Stream.with_index(length)
+    |> Enum.find(fn {list, _index} -> length(Enum.uniq(list)) == length end)
+    |> elem(1)
   end
 end
