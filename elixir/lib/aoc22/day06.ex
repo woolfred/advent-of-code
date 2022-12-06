6e21a50f9d059ac08333b3538f808b9ff52c7fec
Frederik Delißen <frederik.delissen@zweitag.de>
2022-12-06 18:02:25 +0100

    Improve naming

--- elixir/lib/aoc22/day06.ex
+++ elixir/lib/aoc22/day06.ex
@@ -1,14 +1,14 @@
 defmodule AoC22.Day06 do
-  def part_a(input), do: find_unique_chars_marker(input, 4)
+  def part_a(input), do: find_distinct_chars_marker(input, 4)
 
-  def part_b(input), do: find_unique_chars_marker(input, 14)
+  def part_b(input), do: find_distinct_chars_marker(input, 14)
 
-  defp find_unique_chars_marker(input, length) do
+  defp find_distinct_chars_marker(input, length) do
     input
     |> String.to_charlist()
     |> Stream.chunk_every(length, 1, :discard)
     |> Stream.with_index(length)
-    |> Enum.find(fn {list, _index} -> length(Enum.uniq(list)) == length end)
+    |> Enum.find(fn {list, _chars_processed_count} -> length(Enum.uniq(list)) == length end)
     |> elem(1)
   end
 end
